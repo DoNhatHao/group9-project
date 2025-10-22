1d6e0f6eddb527199c93ed851e64a90826b08149
Đỗ Nhật Hào <dnhathao309@gmail.com>
2025-10-22 14:16:36 +0700

    feat(frontend): init CRA + axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { useRef } from 'react';
-import UserList from './components/UserList';
-import AddUser from './components/AddUser';
 
 function App() {
-  const listRef = useRef();
-
   return (
-    <div className="App" style={{padding: '20px'}}>
-      <h1>Frontend - User Manager</h1>
-      <AddUser onAdded={() => { if (listRef.current && listRef.current.fetchUsers) listRef.current.fetchUsers(); }} />
-      <hr />
-      <UserList ref={listRef} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
